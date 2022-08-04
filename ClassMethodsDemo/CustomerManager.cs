19ce951320e07289d00c17cff578eb16fce39291
Gokhan <Gokhan@DESKTOP-G6HREUR>
2022-08-04 11:30:25 +0300

    Add project files.

---
+++ ClassMethodsDemo/CustomerManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodsDemo
+{
+    internal class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerName + " " + customer.CustomerSurname + " : " + "Successfully added to the system");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerName + " " + customer.CustomerSurname + " " + "Successfully deleted from the system");
+        }
+        public void Update(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerName + " " + customer.CustomerSurname + " " + "customer information updated");
+        }
+        public void BalanceUpdade(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerName + " " + customer.Balance + " " + "Balance updated !!");
+        }
+    }
+}
---
+++ ClassMethodsDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMethodsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.CustomerName = "Gokhan";
+            customer1.CustomerSurname = "Gok";
+            customer1.AccountNumber = 123456789;
+            customer1.Balance = 1528.00;
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.CustomerName = "Engin";
+            customer2.CustomerSurname = "Demirog";
+            customer2.AccountNumber = 12345;
+            customer2.Balance = 25045.89;
+
+            Customer[] customers = new Customer[] { customer1, customer2 };
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("Customers in the system " + " " + customer.CustomerName + " " + customer.CustomerSurname + "\n");
+            }
+
+            Console.WriteLine("-----------CustomerManager----------- \n");
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer2);
+            Console.WriteLine();
+            customerManager.Delete(customer2);
+            Console.WriteLine();
+            customerManager.Update(customer1);
+            Console.WriteLine();
+            customerManager.BalanceUpdade(customer1);
+
+        }
+    }
+}
